326b1ca2fa1041b2f5bda2cb83ee981816801995
Rin-t <rin.t.4921@gmail.com>
2025-05-24 16:23:39 +0900

    Initial Commit

---
+++ SampleExchangeCard/SampleExchangeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleExchangeCardApp.swift
+//  SampleExchangeCard
+//
+//  Created by Rin on 2025/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleExchangeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleExchangeCardTests/SampleExchangeCardTests.swift
@@ -0,0 +1,16 @@
+//
+//  SampleExchangeCardTests.swift
+//  SampleExchangeCardTests
+//
+//  Created by Rin on 2025/05/24.
+//
+
+import Testing
+
+struct SampleExchangeCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
